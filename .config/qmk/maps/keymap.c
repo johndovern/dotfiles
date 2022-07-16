8507d3f4097b8701af572e2cc6c86b310796be95
johndovern <92282293+johndovern@users.noreply.github.com>
2022-07-15 20:05:54 -0700

    update 22_07_15-2005-53

--- .config/qmk/maps/keymap.c
+++ .config/qmk/maps/keymap.c
@@ -1,12 +1,15 @@
 #include QMK_KEYBOARD_H
+#include "features/layer_lock.h"
 
 enum custom_keycodes {
     MAC_ZZ = SAFE_RANGE,
     MAC_ZQ,
     MAC_CW,
+    LLOCK,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_layer_lock(keycode, record, LLOCK)) { return false; }
     switch (keycode) {
     case MAC_ZZ:
         if (record->event.pressed) {
@@ -104,49 +107,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define PASTE   C(KC_P)
 #define UNDO    C(KC_Z)
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_5x7(
-        // left hand
-        KC_GRV,     KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_MINS,
-        MH_TAB,     KC_Q,    KC_W,    KC_F,   KC_P,   KC_B,   KC_BSLS,
-        LS_ESC,     ALT_A,   CTL_R,   GUI_S,  LS_T,   MEH_G,  KC_VOLU,
-        KC_LCTL,    BUTT_Z,  KC_X,    MEH_C,  KC_D,   KC_V,
-        TT(_SYMBL), MAC_ZZ,  TAB_L,   SYM_PER,
-                                    KC_LALT, MOU_ESC,
-                                    KC_LCTL, NAV_BCS,
-                                    TG(_MANGA), MED_TAB,
-        // right hand
-                          KC_DEL,  KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,
-                          KC_MINS, KC_J,    KC_L,    KC_U,     KC_Y,     KC_QUOT,  KC_EQL,
-                          KC_VOLD, MEH_M,   LS_N,    GUI_E,    CTL_I,    ALT_O,    MH_ENT,
-                                   KC_K,    KC_H,    MEH_COM,  KC_DOT,   BUTT_SL,  LS_QOUT,
-                                                     SH_LBRC,  SH_RBRC,  MAC_ZQ,   CTL_BSL,
-             SYM_ENT, LS_RBRC,
-             NUM_SPC, KC_LSFT,
-             FUN_DEL, MAC_CW
-    ),
-
-    [_NAV] = LAYOUT_5x7(
-        // left hand
-        _______,   _______,   _______,   _______,   _______,   _______,  _______,
-        _______,   _______,   _______,   _______,   _______,   _______,  _______,
-        TO(_BASE), KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   _______,  _______,
-        _______,   _______,   _______,   _______,   _______,   _______,
-        _______,   _______,   _______,   _______,
-                                    _______,  _______,
-                                    _______,  _______,
-                                    _______,  _______,
-        // right hand
-                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                          _______,   REDO,      PASTE,     COPY,      CUT,       UNDO,      _______,
-                          _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_CAPS,   _______,
-                                     KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    KC_INS,    _______,
-                                                           _______,   _______,   _______,   _______,
-             KC_DEL,  _______,
-             KC_SPC,  _______,
-             KC_ENT,  _______
-    ),
-
     /* Blank Layer
         // left hand
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
@@ -187,12 +147,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              _______, _______
     */
 
-    [_MOUSE] = LAYOUT_5x7(
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_5x7(
+        // left hand
+        KC_GRV,     KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_MINS,
+        MH_TAB,     KC_Q,    KC_W,    KC_F,   KC_P,   KC_B,   KC_BSLS,
+        LS_ESC,     ALT_A,   CTL_R,   GUI_S,  LS_T,   MEH_G,  KC_VOLU,
+        KC_LCTL,    BUTT_Z,  KC_X,    MEH_C,  KC_D,   KC_V,
+        TT(_SYMBL), MAC_ZZ,  TAB_L,   SYM_PER,
+                                    KC_LALT, MOU_ESC,
+                                    KC_LCTL, NAV_BCS,
+                                    TG(_MANGA), MED_TAB,
+        // right hand
+                          KC_DEL,  KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_BSPC,
+                          KC_MINS, KC_J,    KC_L,    KC_U,     KC_Y,     KC_QUOT,  KC_EQL,
+                          KC_VOLD, MEH_M,   LS_N,    GUI_E,    CTL_I,    ALT_O,    MH_ENT,
+                                   KC_K,    KC_H,    MEH_COM,  KC_DOT,   BUTT_SL,  LS_QOUT,
+                                                     SH_LBRC,  SH_RBRC,  MAC_ZQ,   CTL_BSL,
+             SYM_ENT, LS_RBRC,
+             NUM_SPC, KC_LSFT,
+             FUN_DEL, MAC_CW
+    ),
+
+    [_NAV] = LAYOUT_5x7(
         // left hand
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
         TO(_BASE), KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   _______,  _______,
-        _______,   _______,   _______,   _______,   _______,   _______,
+        _______,   _______,   _______,   _______,   LLOCK,  _______,
         _______,   _______,   _______,   _______,
                                     _______,  _______,
                                     _______,  _______,
@@ -200,8 +183,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   REDO,      PASTE,     COPY,      CUT,       UNDO,      _______,
-                          _______,   _______,   KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,   _______,
-                                     _______,   KC_WH_L,   KC_WH_D,   KC_WH_U,   KC_WH_R,   _______,
+                          _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_CAPS,   _______,
+                                     KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    KC_INS,    _______,
+                                                           _______,   _______,   _______,   _______,
+             KC_DEL,  _______,
+             KC_SPC,  _______,
+             KC_ENT,  _______
+    ),
+
+    [_MOUSE] = LAYOUT_5x7(
+        // left hand
+        _______,   _______,   _______,   _______,   _______,   _______,  _______,
+        _______,   _______,   _______,   _______,   _______,   _______,  _______,
+        TO(_BASE), KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   _______,  _______,
+        _______,   _______,   _______,   _______,   LLOCK,     _______,
+        _______,   _______,   _______,   _______,
+                                    _______,  _______,
+                                    _______,  _______,
+                                    _______,  _______,
+        // right hand
+                          _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                          _______,   REDO,      PASTE,     COPY,      CUT,       UNDO,      _______,
+                          _______,   KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,   _______,   _______,
+                                     KC_WH_L,   KC_WH_D,   KC_WH_U,   KC_WH_R,   _______,   _______,
                                                            _______,   _______,   _______,   _______,
              KC_BTN2,  _______,
              KC_BTN1,  _______,
@@ -234,7 +238,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,  _______,
         TO(_BASE), KC_LGUI,   KC_LALT,   KC_LCTL,   KC_LSFT,   _______,  _______,
-        _______,   _______,   _______,   _______,   _______,   _______,
+        _______,   _______,   _______,   _______,   LLOCK,     _______,
         _______,   _______,   _______,   _______,
                                     _______,  _______,
                                     _______,  _______,
@@ -264,7 +268,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   KC_LSFT,   KC_LCTL,   KC_LALT,   KC_LGUI,   _______,
-                                     _______,   _______,   _______,   _______,   _______,   _______,
+                                     _______,   LLOCK,     _______,   _______,   _______,   _______,
                                                            _______,   _______,   _______,   _______,
              _______, _______,
              _______, _______,
@@ -285,7 +289,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   KC_LSFT,   KC_LCTL,   KC_LALT,   KC_LGUI,   _______,
-                                     _______,   _______,   _______,   _______,   _______,   _______,
+                                     _______,   LLOCK,     _______,   _______,   _______,   _______,
                                                            _______,   _______,   _______,   _______,
              _______, _______,
              _______, _______,
@@ -306,7 +310,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   _______,   _______,   _______,   _______,   _______,
                           _______,   _______,   KC_LSFT,   KC_LCTL,   KC_LALT,   KC_LGUI,   _______,
-                                     _______,   _______,   _______,   _______,   _______,   _______,
+                                     _______,   LLOCK,     _______,   _______,   _______,   _______,
                                                            _______,   _______,   _______,   _______,
              _______, _______,
              _______, _______,
